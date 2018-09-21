a597a3fc50bf583789b9fd69114927cd1b6c4538
fibo <casati_gianluca@yahoo.it>
2018-09-21 22:34:01 +0200

    fixed target file path

--- copy.js
+++ copy.js
@@ -13,7 +13,7 @@ if (isInstalledLocally) {
   const fileNameDest = process.argv[3] || fileName
 
   const filePath = path.join(__dirname, fileName)
-  const filePathDest = path.join(process.cwd(), fileNameDest)
+  const filePathDest = path.join(__dirname, '..', '..', fileNameDest)
 
   copyIfItDoesNotExist(filePath, filePathDest)
 }
